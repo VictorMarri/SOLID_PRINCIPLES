f4c89f0d4d80b61b2711095c5b1cca472da1687f
VictorMarri <victorpujoni@gmail.com>
2023-09-16 23:35:47 -0300

    Create 'GerDesc' class. and applying Switch Case to avoid if-else sequence, rename variables and parameters, turn code more legible

---
+++ With_SOLID/With_SOLID/GerenciadorDeDescontos.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace With_SOLID
+{
+    public class GerenciadorDeDescontos
+    {
+        public decimal AplicarDesconto(decimal precoProduto, StatusContaCliente statusTipoContaCliente, int anosDeContaCliente)
+        {
+            decimal precoDepoisDoDesconto = 0;
+            decimal descontoPorFidelidade = (anosDeContaCliente > 5) ? (decimal)5 / 100 : (decimal)anosDeContaCliente / 100;
+
+            switch (statusTipoContaCliente)
+            {
+                case StatusContaCliente.NaoRegistrado:
+                    precoDepoisDoDesconto = precoProduto;
+                    break;
+                case StatusContaCliente.ClienteComum:
+                    precoDepoisDoDesconto = (precoProduto - (0.1m * precoProduto)) - descontoPorFidelidade * (precoProduto - (0.1m * precoProduto));
+                    break;
+                case StatusContaCliente.ClienteEspecial:
+                    precoDepoisDoDesconto = (0.7m * precoProduto) - descontoPorFidelidade * (0.7m * precoProduto);
+                    break;
+                case StatusContaCliente.ClienteVIP:
+                    precoDepoisDoDesconto = (precoProduto - (0.5m * precoProduto)) - descontoPorFidelidade * (precoProduto - (0.5m * precoProduto));
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return precoDepoisDoDesconto;
+        }
+    }
+}
---
+++ With_SOLID/With_SOLID/StatusContaCliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace With_SOLID
+{
+    public enum StatusContaCliente
+    {
+        NaoRegistrado = 1,
+        ClienteComum,
+        ClienteEspecial,
+        ClienteVIP
+    }
+}
