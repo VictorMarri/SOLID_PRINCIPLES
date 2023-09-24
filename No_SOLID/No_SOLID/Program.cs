ed6b6aebc409ae26c8f0d85cd63d4583b9439e2b
VictorMarri <victorpujoni@gmail.com>
2023-09-24 18:01:50 -0300

    Testing Implementation

--- No_SOLID/No_SOLID/Program.cs
+++ No_SOLID/No_SOLID/Program.cs
@@ -6,26 +6,20 @@ internal class Program
     {
         GerDesc gerDesc = new GerDesc();
         Console.WriteLine("Valor da compra 1000 e fidelidade 10 anos (5%)\n");
-
         var resultado = gerDesc.Calcular(1000, 2, 10);
-        Console.WriteLine($"Cliente tipo 2, 10 anos fidelidade,  = {resultado}");
-
+        Console.WriteLine($"Cliente tipo 2, 10 anos fidelidade, ={resultado}");
         var resultado1 = gerDesc.Calcular(1000, 3, 10);
-        Console.WriteLine($"Cliente tipo 3 o valor do desconto é de : {resultado1}");
-
+        Console.WriteLine("Cliente tipo 3 o valor do desconto é de: { resultado1)");
+        
         var resultado2 = gerDesc.Calcular(1000, 4, 10);
-        Console.WriteLine($"Cliente tipo 4 o valor do desconto é de : {resultado2}\n");
-
+        Console.WriteLine($"Cliente tipo 4 o valor do desconto é de :{resultado2}\n");
         Console.WriteLine("Valor da compra 1000 e fidelidade 4 anos (4%)\n");
-        var resultado3 = gerDesc.Calcular(1000, 2, 4);
-        Console.WriteLine($"Cliente tipo 2, 10 anos fidelidade,  = {resultado3}");
-
+        var resultado3 = gerDesc.Calcular(3000, 2, 4);
+        Console.WriteLine($"Cliente tipo 2, 10 anos fidelidade, ={resultado3}");
         var resultado4 = gerDesc.Calcular(1000, 3, 4);
-        Console.WriteLine($"Para Cliente tipo 3 o valor do desconto é de : {resultado4}");
-
+        Console.WriteLine($"Para Cliente tipo 3 o valor do desconto é de: {resultado4}");
         var resultado5 = gerDesc.Calcular(1000, 4, 4);
-        Console.WriteLine($"Para Cliente tipo 4 o valor do desconto é de : {resultado5}");
+        Console.WriteLine($"Para Cliente tipo 4 o valor do desconto é de:{resultado5}");
 
-        Console.ReadLine();
     }
 }
--- With_SOLID/With_SOLID/Program.cs
+++ With_SOLID/With_SOLID/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using With_SOLID.Factory;
+using With_SOLID.Interfaces;
+using With_SOLID;
+using With_SOLID.Descontos.DescontoPorFidelidade;
+using With_SOLID.Enums;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ICalculaDescontoFidelidade descFide = new CalculoDescontoFidelidade();
+        ICalculaDescontoStatusContaFactory descConta = new CalculaDescontoStatusContaFactory();
+
+        GerenciadorDeDescontos gerDesc = new GerenciadorDeDescontos(descFide, descConta);
+
+        Console.WriteLine("Valor da compra 1000 e fidelidade 10 anos (5%)\n");
+
+        var resultado1 = gerDesc.AplicarDesconto(1000, StatusContaCliente.ClienteComum, 10);
+        Console.WriteLine($"Cliente {StatusContaCliente.ClienteComum}, valor do desconto é de: {resultado1}");
+
+        var resultado2 = gerDesc.AplicarDesconto(1000, StatusContaCliente.ClienteEspecial, 10);
+        Console.WriteLine($"Cliente {StatusContaCliente.ClienteEspecial}, valor do desconto é de: {resultado2}");
+
+        var resultado3 = gerDesc.AplicarDesconto(1000, StatusContaCliente.ClienteVIP, 10);
+        Console.WriteLine($"Cliente {StatusContaCliente.ClienteVIP}, valor do desconto é de: {resultado3}");
+
+        Console.WriteLine("\n\nValor da compra 1000 e fidelidade 4 anos (4%)\n");
+        var resultado4 = gerDesc.AplicarDesconto(1000, StatusContaCliente.ClienteComum, 4);
+        Console.WriteLine($"Cliente {StatusContaCliente.ClienteComum}, valor do desconto é de: {resultado4}");
+
+        var resultado5 = gerDesc.AplicarDesconto(1000, StatusContaCliente.ClienteEspecial, 4);
+        Console.WriteLine($"Cliente {StatusContaCliente.ClienteEspecial}, valor do desconto é de: {resultado5}");
+
+        var resultado6 = gerDesc.AplicarDesconto(1000, StatusContaCliente.ClienteVIP, 4);
+        Console.WriteLine($"Cliente {StatusContaCliente.ClienteVIP}, valor do desconto é de: {resultado6}");
+        Console.ReadLine();
+
+    }
+}
